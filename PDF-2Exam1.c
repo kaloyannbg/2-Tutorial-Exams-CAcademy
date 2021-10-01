ea384a62d7b9a866ea122d8c85d1474b6601ce20
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-02 02:01:04 +0300

    Create PDF-2Exam1.c

---
+++ PDF-2Exam1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    double dWeight = 0, dWeightOnTheMoon = 0;
+
+    printf("-- \n\n -- Enter your weight on the earth: "); //User weight on the earth
+
+    scanf("%lf", &dWeight);
+
+    dWeightOnTheMoon = dWeight * 17 / 100;
+
+    printf("\n -- Your weight on the moon is: %.2f", dWeightOnTheMoon);
+
+    return 0;
+}
