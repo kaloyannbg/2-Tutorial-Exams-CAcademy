60bca5976b93a4a270aa6d47e54051a5bf5e7c16
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-02 02:03:02 +0300

    Create PDF2-Exam3.c

---
+++ PDF2-Exam3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    float fEarthDays = 0;
+
+    float fJupiterYears = 0;
+
+    float fEarthYears = 0;
+
+    printf("\n\n -- Convert earth days to jupiter years. -- ");
+
+    printf("\n\n -- Enter earth days: ");
+
+    scanf("%f", &fEarthDays);
+
+    fJupiterYears = fEarthDays / (365 * 12); // jupiterY = 365 / (365*12) earth years jupiterY = 0.083333
+
+    printf("\n\n -- %.2f earth days in jupiter is: %f years.", fEarthDays, fJupiterYears );
+
+    return 0;
+}
