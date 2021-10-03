172d93d0629e3f514c8177588d157b85cedb9372
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-03 13:40:53 +0300

    Create PDF2-Exam4.c

---
+++ PDF2-Exam4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int addDigit(int firstNumber, int secondNumber);
+
+int main() {
+
+    int iNumber = 0; int iNumberTwo = 0;
+
+    iNumber = 4, iNumberTwo = 4;
+
+    printf("%d + %d = %d", iNumber, iNumberTwo, addDigit(iNumber, iNumberTwo));
+
+    return 0;
+}
+
+int addDigit(int firstNumber, int secondNumber) {
+
+    int result = firstNumber + secondNumber;
+
+    return result;
+
+}
